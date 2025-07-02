480c64fe035e8a8e7cfe0f44761c45cde778deb3
Azhar Alvi <96597385+AzharSaeedAlvi@users.noreply.github.com>
2025-07-02 07:21:09 +0000

    Operations in JS

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,52 +1,113 @@
-let score = "33"
-let scoreWithAlphabet = "33abc"
-let scoreWithNull = null
+// /* CONVERSIONS */
+
+// let score = "33"
+// let scoreWithAlphabet = "33abc"
+// let scoreWithNull = null
 
 
-console.log(typeof score);
-// console.log(typeof score);  // To copy the same line as above use the shortcut shift+alt+down
+// console.log(typeof score);
+// // console.log(typeof score);  // To copy the same line as above use the shortcut shift+alt+down
 
-let valueInNumberOne = Number(score)
-let valueInNumberTwo = Number(scoreWithAlphabet)
-let valueInNumberThree = Number(scoreWithNull)
+// let valueInNumberOne = Number(score)
+// let valueInNumberTwo = Number(scoreWithAlphabet)
+// let valueInNumberThree = Number(scoreWithNull)
 
-console.log(typeof valueInNumberOne ) // typeof = Number value = 33
-console.log(valueInNumberOne)
+// console.log(typeof valueInNumberOne ) // typeof = Number value = 33
+// console.log(valueInNumberOne)
 
-console.log(typeof valueInNumberTwo)
-console.log(valueInNumberTwo)     // NOTE - typeof = Number value = NAN (Not a number)
+// console.log(typeof valueInNumberTwo)
+// console.log(valueInNumberTwo)     // NOTE - typeof = Number value = NAN (Not a number)
 
-console.log(typeof scoreWithNull)
-console.log(scoreWithNull)        // NOTE - typeof = Object Value = null
-console.log(valueInNumberThree)  // NOTE -  typeof = Object Value = 0
-
-
-
-/* NOTES:
-"33" => 33 
-"33abc" => NAN
-// true => 1 ; false => 0 
-
-*/
+// console.log(typeof scoreWithNull)
+// console.log(scoreWithNull)        // NOTE - typeof = Object Value = null
+// console.log(valueInNumberThree)  // NOTE -  typeof = Object Value = 0
 
 
 
-let isLoggedIn = 1 
+// /* 
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// NOTES:
+// "33" => 33 
+// "33abc" => NAN
+// // true => 1 ; false => 0 
 
-// "" => false  
-// "any value" => true
-// 1 => true 
+// */
 
 
-let someNumber = 33 
+// let isLoggedIn = 1 
 
-let stringNumber = String(someNumber)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// // "" => false  
+// // "any value" => true
+// // 1 => true 
+
+
+// let someNumber = 33 
+
+// let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+
+
+
+/*  OPERATIONS  */
+
+
+// let value = 3 
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)  // 2^2
+// console.log(2/2)
+// console.log(2%2)  // Remainder 
+
+
+// let str1 = "hello"
+// let str2 = " Azhar"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + "2")  // result in 12 
+// console.log("1" + 2)    // result in 12 
+// console.log( 1+ "2")    // result in 12
+// console.log(1 + "2" + "2") // results in 122   // Explanation can be found under ToPrimitive in documentation  
+// console.log(typeof 1 + "2" + "2")
+// console.log(1 + 2 + "2")  // results in 32  
+
+// console.log(+true);  // output 1
+// console.log(true);   // output true
+// console.log(true+);  // output error
+// console.log(+"");    // output 0
+
+
+// READ - increment and decrement, document in js MDN
+
+let gameCounter = 100
+gameCounter++; 
+console.log(gameCounter);
+
+++gameCounter; 
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
 
 
 
