ddf92e4144611c9464d50a28e0944def48a96ad3
Azhar Alvi <96597385+AzharSaeedAlvi@users.noreply.github.com>
2025-07-06 12:46:28 +0000

    Objects Part Three

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,34 @@
+const course ={
+    coursename:"JS in hindi",
+    price: "999",
+    courseInstructor: "Azhar",
+
+}
+
+// course.courseInstructor 
+
+const{courseInstructor : Instructor} = course  // course is the value the information is being pulled from. courseInstructor is the value that is being pulled from, whereas instructor is the console log that we will utilize. 
+
+console.log(Instructor);
+
+
+// Basics about API. 
+
+
+// {
+//     "name" : "Azhar",
+//     "coursename": "JS in Hindi",
+//     "price": "free" 
+// }
+// This is Json Format, earlier everybody used .xml format but now Json format is used. 
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
+// This is alaso Json format 
+
+
+// Useful pages - randomuser.me and Json formatter to view how things would show up in tree format, text format etc. 
