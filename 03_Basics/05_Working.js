ce8a8dafbacaa82a637ef6a8c19820523413283c
Azhar Alvi <96597385+AzharSaeedAlvi@users.noreply.github.com>
2025-07-08 08:08:26 +0000

    Behind the scene JS

---
+++ 03_Basics/05_Working.js
@@ -0,0 +1,21 @@
+// Code to test in Inspect>Sources>Snippets
+
+
+function one(){
+    console.log("one")
+    two()
+}
+
+function two(){
+    console.log("two");
+    three()
+    
+}
+
+function three(){
+    console.log("three")
+}
+
+one()
+
+// observe the call stack
