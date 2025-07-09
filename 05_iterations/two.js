877bf46d6419e0fc740bf86f159b9a33df665f5c
Azhar Alvi <96597385+AzharSaeedAlvi@users.noreply.github.com>
2025-07-09 06:11:33 +0000

    while do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,33 @@
+// While Loop 
+
+
+// let index = 2
+// while ( index <= 10 ) {
+//     console.log(` Value of index is ${index}`)
+//     index = index + 2;
+// }
+
+
+// let myArray = ["Flash", "batman", "superman"]
+
+// let arr = 0; 
+// while (arr< myArray.length) {
+
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr +1
+    
+// }
+
+
+// +++++++++++ DO WHILE ++++++++++++++
+
+
+let score = 11
+do{ 
+    console.log(` Score is ${score}`);
+    score++; 
+ } while (score <= 10)
+
+
+
+    
